49483c98fdb7763a42a596db0b9e6cfc7de3f089
walidessa <71436871+walidessa@users.noreply.github.com>
2021-06-09 04:07:39 +0200

    Added name and age

--- OneDrive - MBO Utrecht/Text-based-game/src/main/main.kt
+++ OneDrive - MBO Utrecht/Text-based-game/src/main/main.kt
@@ -1,99 +1,31 @@
-fun main(args: Array<String>) {
-    val game=Branch("Je begint in de tijd van de dino’s 30 miljoen jaar geleden. Je loopt rond en je bent aan het observeren en daarna zie je een T-rex. Hij slaapt maar je maakt hem perongeluk wakker. Tip: zoek de magische plant"){
-        WalkNorth() leadsTo TerminalBranch("Level 1:  je moet uit de buurt blijven van de T-rex.")
-        WalkSouth() leadsTo TerminalBranch("Je bent tegen een muur aangelopen, de T-rex heeft je. GAME OVER!")
-        WalkEast() leadsTo Branch("Voor je vind je de plant, eet deze om power te krijgen"){
-            OpenAction() leadsTo TerminalBranch("")
-            val startOfBridgeref:RefBranch=RefBranch(Branch())
-            val startOfBridge=Branch("Je loopt richting een heel oude brug, je staat midden op de brug en kijkt om je heen."){
-                WalkNorth() leadsTo Branch(""){
-                    WalkSouth() leadsTo startOfBridgeref
-                    WalkNorth() leadsTo Branch("Gefeliciteerd je hebt het gehaald!")
-                }
-            }
-            startOfBridgeref.ref=startOfBridge
+fun main (args: Array<String>) {
 
-            WalkNorth() leadsTo startOfBridge
-        }
-    }
-    game.process()
+    println("Welkom bij de T-rex game! \n")
 
-}
+    println("Voer uw naam in:")
+    var username: String? = readline()
 
-interface Action{
-    val label:String
-    fun accept(input :String):Boolean
-}
-
-abstract class SimpleAction(override val label: String):Action{
-    override fun accept(input: String) = label == input
-}
-
-class OpenAction:SimpleAction("OPEN")
-class CloseAction:SimpleAction("DICHT")
-class HitAction:SimpleAction("RAAK")
-
-abstract class WalkAction(val direction:String):Action{
-    override val label: String = "LOOP $direction"
-    override fun accept(input: String): Boolean {
-        val (iaction,idirection)=input.replace(Regex.fromLiteral(" {2,}")," ").split(" ")
-        return iaction=="LOOP" && direction==idirection
-    }
-}
-
-class WalkNorth:WalkAction("Voren")
-class WalkSouth:WalkAction("Achter")
-class WalkEast:WalkAction("Links")
-class WalkWest:WalkAction("Rechts")
-
-interface IBranch {
-    val description: String
-    val fn: IBranch.() -> Unit
-    fun process()
-
-    infix fun Action.leadsTo(brc: IBranch)
-}
-
-
-open class Branch(override val description:String="", override val fn:IBranch.()->Unit={}) : IBranch {
-    val actions:MutableList<Pair<Action,IBranch>> = mutableListOf()
-    var visited=false
-    override fun process()  {
-        if (!visited) this.fn()
-        visited=true
-
-        println(description)
-        print("Het volgende is aanwezig:  \n- ${actions.map { it.first.label }.joinToString("\n- ")}\n> ")
-        var input= readLine() ?: ""
-        var processed  = actions.find { it.first.accept(input) }?.second?.run { process(); true }?:false
-        while (!processed){
-            input= readLine() ?: ""
-            processed = actions.find { it.first.accept(input) }?.second?.run { process(); true }?:false
-        }
-    }
-    override infix fun Action.leadsTo(brc:IBranch){
-        actions.add(this to brc)
-    }
-}
-
-class RefBranch(var ref:Branch=Branch()):IBranch {
-    override val fn: IBranch.() -> Unit
-        get() = ref.fn
-    override val description: String
-        get() = ref.description
-
-    override fun process() {
-        ref.process()
+    while (username!!.isBlank()) {
+        println("Dit veld is verplicht!")
+        username = readline()
     }
 
-    override fun Action.leadsTo(brc: IBranch) {
-        ref.actions.add(this to brc)
+    println("Welkom" + username + "! \n")
+
+    println("Vul uw leeftijd in:")
+    val age = readline()!!.toInt()
+    var underage: Int = 16
+
+    if (age < underage) {
+        println("U bent jammer genoeg te jong om dit spel te mogen spelen, kom later terug")
+    } else {
+        println("Welkom" + username + "van" + age + " jaar \n")
+
     }
 
-}
+    println("Zo'n 4,5 tot 4,6 miljard jaar geleden was de oerknal en kwam de aarde tot stand.")
+    println("Naarmate de tijd werd de aarde steeds voller en mooier, jammer genoeg ook gevaarlijker.")
+    println("De dinosaurussen begonnen uit te sterven behalve de T-rex, die zijn bijna onverwoestbaar zou je zeggen.")
+    println("Tot er een vulkaanuitbarsting was, de T-rexen begonnen zich te schuilen en waren inmiddels al 400 jaar aan het slapen.")
 
-class TerminalBranch(val ending:String):Branch(){
-    override fun process(){
-        println(ending)
-    }
 }
